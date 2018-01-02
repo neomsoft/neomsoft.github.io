069aeb9bf6f08d951e7b50b8e7397d31e2688477
Yura <neomsoft@github.com>
2018-01-02 10:33:03 +0200

    bitc v2

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var request = new XMLHttpRequest();
+request.open('GET', 'https://api.coinmarketcap.com/v1/ticker/bitcoin/');
+
+request.onload = function(e) {
+	var response = JSON.parse(this.responseText);
+	document.getElementById("usd").innerHTML = response[0].price_usd;
+}
+
+request.send(null);
