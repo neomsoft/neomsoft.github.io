598ce15f5e4228a812903b7a4623505eae5ec77b
Yura <34751528+neomsoft@users.noreply.github.com>
2018-03-07 16:25:39 +0200

    n

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var request = new XMLHttpRequest();
-request.open('GET', 'https://api.coinmarketcap.com/v1/ticker/bitcoin/');
-
-request.onload = function(e) {
-	var response = JSON.parse(this.responseText);
-	document.getElementById("course").innerHTML = Math.ceil(response[0].price_usd) + ' $';
-}
-
-request.send(null);
