a47300cbb8b98c31ff7db8b9459b4710df338f20
Yura <34751528+neomsoft@users.noreply.github.com>
2025-06-16 21:56:36 +0200

    style

--- projects/bookmarks/main.js
+++ projects/bookmarks/main.js
@@ -1,18 +1,14 @@
 var body = "";
+var xmlhttp = new XMLHttpRequest();
 
-function loadXMLDoc() {
-	var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		createBoby(this);
+	}
+};
 
-	xmlhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			createBoby(this);
-		}
-	};
-
-
-	xmlhttp.open("GET", "reading.xml", true);
-	xmlhttp.send();
-}
+xmlhttp.open("GET", "reading.xml", true);
+xmlhttp.send();
 
 function createBoby(xml) {
 	var xmlDoc = xml.responseXML;
@@ -23,34 +19,51 @@ function createBoby(xml) {
 	}
 
 	document.body.innerHTML += body;
+
+	addOnClicks();
 }
 
 function addElements(node) {
 	if (node.nodeName == 'folder') {
-		body += '<div class="folder">';
+		body += '<div>';
+
+		body += '<div type="button" class="folder_title">';
+		body += node.getElementsByTagName('title')[0].textContent;
+		body += '</div>';
+
+		body += '<div class="content">';
 
 		for (var i = 0; i < node.childNodes.length; i++) {
 			addElements(node.childNodes[i]);
 		}
 
 		body += '</div>';
-	}
-
-	if (node.nodeName == 'title') {
-		body += '<h4>';
-		body += node.textContent;
-		body += '</h4>';
+		body += '</div>';
 	}
 
 	if (node.nodeName == 'bookmark') {
-		body += '<div class="bookmark">';
-		body += '<a class="button" href="';
+		body += '<a href="';
 		body += node.attributes['href'].textContent;
 		body += '">';
+		body += '<div class="bookmark">';
 		body += node.getElementsByTagName('title')[0].textContent;
+		body += '</div>';
 		body += '</a>';
-		body += '</div	>';
 	}
 }
 
-loadXMLDoc();
+function addOnClicks() {
+	var titles = document.getElementsByClassName("folder_title");
+
+	for (var i = 0; i < titles.length; i++) {
+		titles[i].addEventListener("click", function() {
+			var content = this.nextElementSibling;
+			
+			if (content.style.display === "block") {
+				content.style.display = "none";
+			} else {
+				content.style.display = "block";
+			}
+		});
+	}
+}
